c04e14c036aab3c9e8d11dcec705894e120aa004
nghtkhang <nghtkhang@gmail.com>
2014-12-05 10:57:13 +0700

    init

--- HelloWorld/HelloWorld/HelloWorld/bld/Debug/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\Khang\Documents\Visual Studio 2015\Projects\HelloWorld\HelloWorld\HelloWorld\bld\Debug\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
-C:\Users\Khang\Documents\Visual Studio 2015\Projects\HelloWorld\HelloWorld\HelloWorld\bld\Debug\platforms\android\ant-build\CordovaApp.ap_ \
-C:\Users\Khang\Documents\Visual Studio 2015\Projects\HelloWorld\HelloWorld\HelloWorld\bld\Debug\platforms\android\ant-build\classes.dex \
-C:\Users\Khang\Documents\Visual Studio 2015\Projects\HelloWorld\HelloWorld\HelloWorld\bld\Debug\platforms\android\CordovaLib\ant-build\classes.jar \
--- HelloWorld/HelloWorld/bld/Debug/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Khang\Documents\Visual Studio 2015\Projects\HelloWorld\HelloWorld\bld\Debug\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\Khang\Documents\Visual Studio 2015\Projects\HelloWorld\HelloWorld\bld\Debug\platforms\android\CordovaLib\AndroidManifest.xml \
